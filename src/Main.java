9a3749512c38a0f8797052d8e7672e0a4a320f66
Kennethc203 <kennethc203@nycstudents.net>
2025-10-16 15:35:19 -0400

    AHHHHHHHHHHHHHHHHHHHHHHHHH

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
+        /*
         boolean[][] schedule = new boolean[8][60];
         AppointmentBook somethingOrOther = new AppointmentBook(schedule);
         for(int i = 10; i < 15; i++) {schedule[1][i] = true;}
@@ -20,6 +24,39 @@ public class Main {
         why.printPeriod(4);
         why.makeAppointment(2, 4, 22);
         why.printPeriod(4);
+         */
+        AppointmentBook[] books = new AppointmentBook[1000];
+        File paper = new File("schedules.txt");
+        Scanner notPrinter = new Scanner(paper);
+        boolean[][][] superSchedule = new boolean[1000][8][60];
+        int totalAppointments = 0;
+        int min;
+        int hour;
+        int periodBegin;
+        int periodEnd;
+        int periodMinutes;
+        for (int i = 0; i < 1000; i++) {
+            books[i] = new AppointmentBook(superSchedule[i]);
+            min = 0;
+            hour = 0;
+            for (int j = 0; j <= 480; j++) {
+                superSchedule[i][hour][min] = notPrinter.nextBoolean();
+                min++;
+                if (min == 60) {
+                    min = 0;
+                    hour++;
+                    if (hour > 7) hour = 7;
+                }
 
+
+            periodBegin = notPrinter.nextInt();
+            periodEnd = notPrinter.nextInt();
+            periodMinutes = notPrinter.nextInt();
+            if (books[i].makeAppointment(periodBegin, periodEnd, periodMinutes)) {
+                totalAppointments++;
+                }
+            }
+            System.out.println(totalAppointments);
+        }
     }
 }
